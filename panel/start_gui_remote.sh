c5a6dd1246716eb6532fee0a2192eba115bf1a4a
HoriFox <lhorifoxl@gmail.com>
2024-09-01 22:42:49 +0300

    Привёл в скрипт запуска на удалённом АРМ

--- panel/start_gui_remote.sh
+++ panel/start_gui_remote.sh
@@ -1,10 +1,14 @@
 #!/bin/bash
 
+USER=root
+REMOTE_IP=192.168.31.33
+
+
 echo "> Stop panel service..."
-ssh root@192.168.31.33 "systemctl stop panel"
+ssh $USER@$REMOTE_IP "systemctl stop panel"
 echo "> Remove panel files..."
-ssh root@192.168.31.33 "rm -rf /home/user/panel_tmp; mkdir /home/user/panel_tmp"
+ssh $USER@$REMOTE_IP "rm -rf /home/user/panel_tmp; mkdir /home/user/panel_tmp"
 echo "> Copy panel files..."
-scp -r * root@192.168.31.33:/home/user/panel_tmp > /dev/null
+scp -r * $USER@$REMOTE_IP:/home/user/panel_tmp > /dev/null
 echo "> Start panel service..."
-ssh root@192.168.31.33 "systemctl start panel"
+ssh $USER@$REMOTE_IP "systemctl start panel"
